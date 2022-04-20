4071554f9577dac898131b4c16128ac49af97f91
KoT OsKaR <tibeco255@yandex.ru>
2022-04-20 19:49:43 +0300

    update

--- main/models.py
+++ main/models.py
@@ -30,7 +30,7 @@ class Article(Model):
 
     def save(self, *args, **kwargs):
         new = False
-        if not Article.objects.filter(name = self.name, info = self.info, image = self.image, date = self.date).exists():
+        if self.id == None:
             new = True
         try:
             super().save(*args, **kwargs)
